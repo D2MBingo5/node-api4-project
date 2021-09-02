8d7c2635b9720999707078e5a3654829a2c2dae8
Leu Family <romanianfobjr@gmail.com>
2021-09-02 15:57:23 -0500

    project created

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 5000
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
